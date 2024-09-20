d9ea0ec7e3daff5f67b6cd49e1af957603a6c7d5
kai <devkai4@pm.me>
2024-09-20 18:12:49 +0900

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-user_prompt = "Enter a todo:
+user_prompt = "Enter a todo:"
 todo1 = input(user_prompt)
 todo2 = input(user_prompt)
 todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3, "Hello"] # list
+todos = [todo1, todo2, todo3, "Hello"] #list
 print(todos)
 
 print(type(todo1))
