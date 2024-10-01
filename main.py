d61c4776eac86804ce4ae2a0915fed5e5b0fcca4
devkai <devkai4@pm.me>
2024-10-01 14:20:10 +0900

    Optimising the Code

--- main.py
+++ main.py
@@ -5,17 +5,14 @@ while True:
     match user_action:
         case 'add':
             todo = input("Enter a todo: ") + "\n"
-            file = open('todos.txt','r')
-            todos = file.readlines()
-            file.close()
+            with open('todos.txt','r') as file:
+                todos = file.readlines()
             todos.append(todo)
-            file = open('todos.txt','w')
-            file.writelines(todos)
-            file.close()
+            with open('todos.txt','w') as file:
+                file.writelines(todos)      
         case 'show':
-            file = open('todos.txt','r')
-            todos = file.readlines()
-            file.close()
+            with open('todos.txt','r') as file:
+                todos = file.readlines()
             for index, item in enumerate(todos):
                 item = item.strip('\n')
                 row = f"{index + 1}-{item}"
