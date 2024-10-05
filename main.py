3acd9c18d8a0cd92a72b8a79bf58489457937aa5
devkai4 <devkai4@pm.me>
2024-10-05 20:08:15 +0900

    Fixing Two Bugs in the Program

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ while True:
     user_action = input("Type add or show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    if 'add' in user_action:
-        todo = user_action[4:] + "\n" # list slicing operation
+    if user_action.startswith('add'):
+        todo = user_action[4:] # list slicing operation
         with open('todos.txt','r') as file:
             todos = file.readlines()
-        todos.append(todo)
+        todos.append(todo + '\n')
         with open('todos.txt','w') as file:
             file.writelines(todos)      
-    elif 'show' in user_action:
+    elif user_action.startswith('show'):
         with open('todos.txt','r') as file:
             todos = file.readlines()
         for index, item in enumerate(todos):
             item = item.strip('\n')
             row = f"{index + 1}-{item}"
             print(row)
-    elif 'edit' in user_action:
+    elif user_action.startswith('edit'):
         number = user_action[5:]
         number = number - 1
         with open('todos.txt','r') as file:
@@ -25,7 +25,7 @@ while True:
         todos[number] = new_todo + '\n'
         with open('todos.txt','w') as file:
             file.writelines(todos)
-    elif 'complete' in user_action:
+    elif user_action.startswith('complete'):
         number = int(user_action[9:]) # list slicing operation
         with open('todos.txt','r') as file:
             todos = file.readlines()
@@ -36,7 +36,7 @@ while True:
             file.writelines(todos)
         message = f"Todo {todo_to_remove} has been removed from the list."
         print(message)
-    elif 'exit' in user_action:
+    elif user_action.startswith('exit'):
         break
     else:
         print("Command is not valid.")
