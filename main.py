8ad0b751bba204e2bbd05a33d64426d764a93e90
devkai4 <devkai4@pm.me>
2024-10-06 19:10:20 +0900

    Anticipating Program Errors

--- main.py
+++ main.py
@@ -17,25 +17,34 @@ while True:
             row = f"{index + 1}-{item}"
             print(row)
     elif user_action.startswith('edit'):
-        number = user_action[5:]
-        number = number - 1
-        with open('todos.txt','r') as file:
-            todos = file.readlines()
-        new_todo = input("Enter new todo: ")
-        todos[number] = new_todo + '\n'
-        with open('todos.txt','w') as file:
-            file.writelines(todos)
+        try:
+            number = int(user_action[5:])
+            print(number)
+            number = number - 1
+            with open('todos.txt','r') as file:
+                todos = file.readlines()
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo + '\n'
+            with open('todos.txt','w') as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Your command is not valid.")
+            continue
     elif user_action.startswith('complete'):
-        number = int(user_action[9:]) # list slicing operation
-        with open('todos.txt','r') as file:
-            todos = file.readlines()
-        index = number - 1
-        todo_to_remove = todos[index].strip('\n')
-        todos.pop(index)
-        with open('todos.txt','w') as file:
-            file.writelines(todos)
-        message = f"Todo {todo_to_remove} has been removed from the list."
-        print(message)
+        try:
+            number = int(user_action[9:]) # list slicing operation
+            with open('todos.txt','r') as file:
+                todos = file.readlines()
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+            with open('todos.txt','w') as file:
+                file.writelines(todos)
+            message = f"Todo {todo_to_remove} has been removed from the list."
+            print(message)
+        except IndexError:
+            print("There is no item with that number.")
+            continue
     elif user_action.startswith('exit'):
         break
     else:
