405f31885bc6141fd3ed97c0b1776eebc125ed00
devkai4 <devkai4@pm.me>
2024-10-09 19:05:05 +0900

    Optimising the Code Further

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ def get_todos(filepath):
         todos_local = file_local.readlines()
     return todos_local
 
+def write_todos(filepath, todos_arg):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
 while True:
     user_action = input("Type add or show, edit, complete or exit: ")
     user_action = user_action.strip()
@@ -10,8 +14,7 @@ while True:
         todo = user_action[4:] 
         todos = get_todos("todos.txt")
         todos.append(todo + '\n')
-        with open('todos.txt','w') as file:
-            file.writelines(todos)      
+        write_todos("todos.txt", todos)    
     elif user_action.startswith('show'):
         todos = get_todos("todos.txt")
         for index, item in enumerate(todos):
@@ -28,8 +31,7 @@ while True:
                 todos = file.readlines()
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
-            with open('todos.txt','w') as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -40,8 +42,7 @@ while True:
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
-            with open('todos.txt','w') as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos)
             message = f"Todo {todo_to_remove} has been removed from the list."
             print(message)
         except IndexError:
